792f69329d8470b46f37190c96b107e33c1ff0e1
fogus <mefogus@gmail.com>
2012-07-10 07:42:49 -0400

    scaffolding of the emitter

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Experiments in compiling Clojure to Java."
   :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]
                  [org.clojure/clojurescript "0.0-1443"]
-                 [org.clojure/core.unify "0.5.3"]]
+                 [org.clojure/core.unify "0.5.3"]
+                 [cljv/clj-soy "0.3.0"]]
   :dev-dependencies [[jline "0.9.94"]
                      [swank-clojure "1.4.2"]
                      [lein-marginalia "0.7.1"]]
--- src/clj/cljv/java.clj
+++ src/clj/cljv/java.clj
@@ -1,4 +1,22 @@
 (ns cljv.java
   (:require [cljv.util :as util]))
 
+(def reserved
+  #{"abstract" "assert"
+    "boolean" "break" "byte"
+    "case" "catch" "char" "class" "const" "continue"
+    "default" "do" "double"
+    "else" "enum" "extends"
+    "false" "final" "finally" "float" "for"
+    "goto"
+    "if" "implements" "import" "instanceof" "int" "interface"
+    "long"
+    "native" "new" "null"
+    "package" "private" "protected" "public"
+    "return"
+    "short" "static" "strictfp" "super" "switch" "synchronized"
+    "this" "throw" "throws" "transient" "true" "try"
+    "void" "volatile"
+    "while"})
+
 (def rename-to-java #(util/rename-as % #"\.cljv$" ".java"))
