bbb6c6ccd28f934900fe000e39051826b33817b9
fogus <mefogus@gmail.com>
2012-07-19 07:56:51 -0400

    WIP: working one building the parse tree. Needs sub-node parsing and complete top level still

--- src/clj/cljv/parser.clj
+++ src/clj/cljv/parser.clj
@@ -7,35 +7,112 @@
 
 (def ^:dynamic *position* nil)
 
-(defmacro parse-wrap [env & body]
-  `(let [env# ~env]
-     (with-meta
-       ~@body
-       {:context (:context env#)})))
-
-(defn emit-block
-  [context statements ret]
-  (when statements
-    (emits statements))
-    (emit ret))
-
 (defmethod parse :constant
   [{:keys [form env]}]
-  (when-not (= :statement (:context env))
-    (parse-wrap
-     env
-     {:node  :constant
-      :value form})))
+  {:node :constant
+   :value form
+   :context (:context env)})
 
 (defmethod parse :no-op [_])
-(defmethod parse :var [_])
-(defmethod parse :meta [_])
-(defmethod parse :map [_])
-(defmethod parse :vector [_])
-(defmethod parse :set [_])
-(defmethod parse :if [_])
-(defmethod parse :throw [_])
-(defmethod parse :def [_])
+
+(defmethod parse :var
+  [{:keys [info env]}]
+  (let [n (:name info)]
+    {:node :var
+     :name n
+     :namespace (namespace n)
+     :context (:context env)
+     :env env}))
+
+(defmethod parse :meta
+  [{:keys [expr meta env]}]
+  {:node :meta
+   :context (:context env)
+   :expr expr
+   :meta meta
+   :env env})
+
+(defmethod parse :map
+  [{:keys [env simple-keys? keys vals]}]
+  {:node :map
+   :context (:context env)
+   :env env
+   :simple-keys? simple-keys?
+   :keys keys
+   :keys-sz (count keys)
+   :vals vals
+   :vals-sz (count vals)})
+
+(defmethod parse :vector
+  [{:keys [items env]}]
+  {:node :vector
+   :context (:context env)
+   :env env
+   :items items
+   :items-sz (count items)})
+
+(defmethod parse :set
+  [{:keys [items env]}]
+  {:node :set
+   :context (:context env)
+   :items items
+   :items-sz (count items)
+   :env env})
+
+(defn get-tag [e]
+  (or (-> e :tag)
+      (-> e :info :tag)))
+
+(defn infer-tag [e]
+  (if-let [tag (get-tag e)]
+    tag
+    (case (:op e)
+      :let (infer-tag (:ret e))
+      :if (let [then-tag (infer-tag (:then e))
+                else-tag (infer-tag (:else e))]
+            (when (= then-tag else-tag)
+              then-tag))
+      :constant (case (:form e)
+                  true 'boolean
+                  false 'boolean
+                  nil)
+      nil)))
+
+(defn safe-test? [e]
+  (let [tag (infer-tag e)]
+    (or (#{'boolean 'seq} tag)
+        (when (= (:op e) :constant)
+          (let [form (:form e)]
+            (not (or (and (string? form) (= form ""))
+                                          (and (number? form) (zero? form)))))))))
+
+(defmethod parse :if
+  [{:keys [test then else env unchecked]}]
+  {:node :if
+   :context (:context env)
+   :test test
+   :checked (not (or unchecked (safe-test? test)))
+   :then then
+   :else else
+   :env env})
+
+(defmethod parse :throw
+  [{:keys [throw env]}]
+  {:node :throw
+   :context (:context env)
+   :env env
+   :throw throw})
+
+(defmethod parse :def
+  [{:keys [name init env doc export]}]
+  {:node :def
+   :context (:context env)
+   :env env
+   :name name
+   :doc doc
+   :jvdoc (:jsdoc init)
+   :export export})
+
 (defmethod parse :fn [_])
 (defmethod parse :do [_])
 (defmethod parse :try* [_])
@@ -46,9 +123,10 @@
 
 (defmethod parse :new
   [{:keys [ctor args env]}]
-  (parse-wrap env
-    {:node :new
-     :args args}))
+  {:node :new
+   :args args
+   :context (:context env)
+   :env env})
 
 (defmethod parse :set! [_])
 (defmethod parse :ns [_])
